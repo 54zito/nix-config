1552d37e2443ec5eb9e4ad2da7db8b0a2d9d344e
54zito <zito@ee-f.com>
2025-09-12 20:21:27 +0000

    nix-command and flakes are now activated in installer, added git and disko to pkgs

--- flakeHelper.nix
+++ flakeHelper.nix
@@ -5,10 +5,11 @@
       modules = [
         ({ pkgs, modulesPath, ... }: {
           imports = [ (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix") ];
+          nix.settings.experimental-features = "nix-command flakes";
           services.openssh.enable = true;
           systemd.services.sshd.wantedBy = pkgs.lib.mkForce [ "multi-user.target" ];
           users.users.root.openssh.authorizedKeys.keys = sshKeys;
-          environment.systemPackages = with pkgs; [ exfatprogs pciutils htop neofetch ];
+          environment.systemPackages = with pkgs; [ disko exfatprogs pciutils git htop neofetch ];
         })
       ];
     };
